f6ca3f0b0a3d141e253349a639d4266c0502789a
Timothy Yang <timyang@mit.edu>
2012-03-25 05:07:43 -0400

    Gets JSON information from places near cat and updates as cat moves

--- static/js/nyanmap.js
+++ static/js/nyanmap.js
@@ -8,27 +8,42 @@ function initialize(lat,lng) {
     map = new google.maps.Map(document.getElementById("map_canvas"),
 				  myOptions);
 
-    https://nyancat.ninjapiraterockstardeveloper.com/google/places/?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AddYourOwnKeyHere
+    //remove all this hardcoded stuff later, it's just for testing getting the JSON info
     var radius = 500;
     var types = "food";
-    var places_url = "https://nyancat.ninjapiraterockstardeveloper.com/google/places?location="+lat+","+lng+"&radius="+radius+"&types="+types+"&sensor=true&key=AIzaSyDVlmhMFLkex9hygFh8POvX7JwUAvdyX9s";
-
+    var places_url = "http://nyancat.ninjapiraterockstardeveloper.com/google/places?location="+lat+","+lng+"&radius="+radius+"&types="+types+"&sensor=true&key=AIzaSyDVlmhMFLkex9hygFh8POvX7JwUAvdyX9s";
     var location_data = $.getJSON(places_url);
-
     console.log(location_data);
+
+
+
 
 //updates center when user drags the map
 
     google.maps.event.addListener(map, 'center_changed', function() {
 	cat_location = map.getCenter();
 	console.log(cat_location);
+	nearby_locations = [] //clear stored json
+
+	var radius = 500; //meters
+	var types = "points_of_interest";
+	var lat = cat_location.lat;
+	var lng = cat_location.lng;
+	var places_url = "http://nyancat.ninjapiraterockstardeveloper.com/google/places?location="+lat+","+lng+"&radius="+radius+"&types="+types+"&sensor=true&key=AIzaSyDVlmhMFLkex9hygFh8POvX7JwUAvdyX9s";//don't put actual api key
+	nearby_locations = $.getJSON(places_url).results;//ask server for json data, stores in nearby in api.js
+	console.log(nearby_locations);
+	//function that loads markers from json data
     });
+}
 
 
-
+var place_markers = function()
+{
 
 }
 
+
+//deprecated
 var panToCat = function()
 {
     var lat = 10;

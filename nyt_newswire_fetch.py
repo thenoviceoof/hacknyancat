822561365efaf53cee326326ab37811c414444d6
Nathan Hwang <beta.entity.k@gmail.com>
2012-03-24 22:12:12 -0400

    Commiting simple newswire cron job

--- nyt_newswire_fetch.py
+++ nyt_newswire_fetch.py
@@ -3,11 +3,18 @@
 # this fetches NYT newswire stuff every 20 minutes (duck under the
 # 5000 daily rate limit
 
+import requests
+import json
+
 from config import NYT_API_KEY
 
 API_KEY = NYT_API_KEY
 
 # {version}/content/src/sect/hrs.format?api-key=#
-url = "http://api.nytimes.com/svc/news/{version}/content/\
-all/all/10.json?api-key={api-key}"
+"http://api.nytimes.com/svc/news/v3/content/all/all/24.json?api-key=####"
+url = "http://api.nytimes.com/svc/news/v3/content/\
+all/all/24.json?api-key={apikey}".format(apikey=API_KEY)
 
+r = requests.get(url)
+print(r.content)
+j = json.loads(r.content)

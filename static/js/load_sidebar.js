7a906df87bbfa2661a0f10fc16b89ff8f8bd2e50
Nathan Hwang <beta.entity.k@gmail.com>
2012-03-25 12:42:22 -0400

    Tweaking changes

--- static/js/load_sidebar.js
+++ static/js/load_sidebar.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
 
 function load_sidebar() {
     // etsy
-    $("#etsy-sidebar")[0].contentDocument.location.reload(true);
-    $("#dc-sidebar")[0].contentDocument.location.reload(true);
-    $("#longtail-sidebar")[0].contentDocument.location.reload(true);
+    // $("#etsy-sidebar")[0].contentDocument.location.reload(true);
+    // $("#dc-sidebar")[0].contentDocument.location.reload(true);
+    // $("#longtail-sidebar")[0].contentDocument.location.reload(true);
 
     // $.ajax({
     //     url: '/etsy-results/',

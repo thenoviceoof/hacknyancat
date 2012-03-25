22e32dff1c5f96f66886980485e651726a13896d
david <david@celeste.(none)>
2012-03-25 01:10:22 -0400

    Updated server and template for Foursquare

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
 from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template
+from flask import request as request_handler
 
 import pymongo
 from pymongo import Connection
 
-from config import SECRET_KEY
+import urllib
+
+#from config import SECRET_KEY
+
+SECRET_KEY = "key"
 
 ################################################################################
 
 # defaults to /static and /templates
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
+
+# API ids
+
+#Foursquare
+FS_CLIENT_ID = "IGHYIIQIRRVUGIMJ05XGMGXIGH25WVZ5CLFM53C5BXXQXHTN"
+FS_CLIENT_SECRET = "E4EATPZBLVLMTJSIUZRK3T2O3UGR4IARF50UP35Q0Y0KJ5TP"
+FS_REDIRECT_URI = urllib.urlencode([("redirect_uri","http://ninjapiraterockstardeveloper.com/apifeedback/foursquare")])
+
+
 
 @app.route('/')
 def index():
@@ -24,6 +38,13 @@ def test_api():
     if not('user' in session):
         pass
     return render_template('test_api.json')
+
+@app.route('/apifeedback/foursquare')
+def process_facebook():
+    usercode = request_handler.args.get('code')
+
+    request_url = "https://foursquare.com/oauth2/access_token?client_id=%s&client_secret=%s&grant_type=authorization_code&redirect_uri=%s&code=CODE", FS_CLIENT_ID, FS_CLIENT_SECRET, FS_REDIRECT_URI
+    return ""
 
 if __name__=="__main__":
     app.run(debug=True)

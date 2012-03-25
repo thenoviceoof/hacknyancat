f8a4a62d6fcb02e3ed2dbe769e6a7436bb44b156
Nathan Hwang <beta.entity.k@gmail.com>
2012-03-25 11:49:29 -0400

    load sidebar function

--- static/js/load_sidebar.js
+++ static/js/load_sidebar.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
     (function($) {
         // load up data into appropriate sections
-
-        // // etsy
-        // $.ajax({
-        //     url: '/etsy-results/',
-        //     dateType: "html",
-        //     success: function(data, textStatus) {
-        //         $("#etsy-sidebar").html(data);
-        //     }
-        // });
-
-        // // dc
-        // $.ajax({
-        //     url: '/dc-results/',
-        //     dateType: "html",
-        //     success: function(data, textStatus) {
-        //         $("#dc-sidebar").html(data);
-        //     }
-        // });
-
-        // // dc
-        // $.ajax({
-        //     url: '/static/longtail.html',
-        //     dateType: "html",
-        //     success: function(data, textStatus) {
-        //         $("#dc-sidebar").html(data);
-        //     }
-        // });
+        load_sidebar();
     })(jQuery);
 });
+
+function load_sidebar() {
+    // etsy
+    $.ajax({
+        url: '/etsy-results/',
+        dateType: "html",
+        success: function(data, textStatus) {
+            $("#etsy-sidebar").html(data);
+        }
+    });
+
+    // dc
+    $.ajax({
+        url: '/dc-results/',
+        dateType: "html",
+        success: function(data, textStatus) {
+            $("#dc-sidebar").html(data);
+        }
+    });
+
+    // dc
+    $.ajax({
+        url: '/static/longtail.html',
+        dateType: "html",
+        success: function(data, textStatus) {
+            $("#dc-sidebar").html(data);
+        }
+    });
+}

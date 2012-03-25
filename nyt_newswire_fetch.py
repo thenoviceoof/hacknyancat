598b5d9fea4e9cb7f832919cf6c5a4de99c722a2
Nathan Hwang <beta.entity.k@gmail.com>
2012-03-25 00:52:10 -0400

    Added parse.ly hook

--- nyt_newswire_fetch.py
+++ nyt_newswire_fetch.py
@@ -5,16 +5,44 @@
 
 import requests
 import json
+import time
 
-from config import NYT_API_KEY
+from config import NYT_API_KEY, PARSELY_API_URL
 
 API_KEY = NYT_API_KEY
 
+# get 20 
 # {version}/content/src/sect/hrs.format?api-key=#
 "http://api.nytimes.com/svc/news/v3/content/all/all/24.json?api-key=####"
 url = "http://api.nytimes.com/svc/news/v3/content/\
-all/all/24.json?api-key={apikey}".format(apikey=API_KEY)
+all/all/24.json?api-key={apikey}&offset={offset}".format(apikey=API_KEY,
+                                                         offset=0)
 
 r = requests.get(url)
 print(r.content)
 j = json.loads(r.content)
+
+results = j["results"]
+titles = [res["title"] + ": " + res["abstract"] for res in results]
+title_str = " | ".join(titles)
+print(titles)
+
+########################################
+# join titles, push up to parse.ly
+
+r = requests.post(PARSELY_API_URL+"parse", {"text": titles})
+print(r.content)
+status_url = json.loads(r.content)["url"][1:]
+
+i = 1
+r = requests.get(PARSELY_API_URL+status_url)
+print(r.content)
+status = json.loads(r.content)
+while status["status"] != "DONE":
+    i *= 2
+    time.sleep(i)
+    # reget the parse
+    r = requests.get(PARSELY_API_URL+status_url)
+    status = json.loads(r.content)
+    print(r.content)
+print(status["data"])
